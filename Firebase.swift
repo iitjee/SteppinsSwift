08e702772f7e68224bfd4d172342145db499aa14
iitjee <iit.jee.help.me@gmail.com>
2016-10-22 03:03:05 +0530

    Create Firebase.swift

---
+++ Firebase.swift
@@ -0,0 +1,25 @@
+/*  FIREBASE:
+
+  1. Helps you create, market, and secure your iOS apps. :)
+  2. Initially, it was just a cloud database for mobile apps but now it's encompassing many more for both native and web based!!
+  
+  3. Firebase has 3 categories:
+    -Develop  -> Measure App usage and demographic info, allow SignUp/Login via many Identity Providers, Detect Crashes, Notifications!
+    -Grow     -> 
+    -Earn
+  Firebase Analytics encompasses all the above 3 and tightly integrates with most of the other APIs.
+
+  4. Firebase Cloud Messaging (FCM) allows to deliveer notifications; and send and receive messages to and fro from users
+     Firebase Authentication allows users to signup/login to your app
+     Firebase Realtime Database allows
+     Firebase Hosting proves CDN (Content Delivery Network) to make ur data available worldwide as fast as possible
+     Firebase RemoteConfiga allows you deliver custom UX to users without requiring them to intall/download the update
+     Firebase Testlab allows you to write hundreds of Tests to test number of device configurations to expose bugs
+     Firebase CrashReporting to keep your app stable
+
+
+*/
+
+pod 'Firebase/Core' //Analytics pkg
+pod 'Firebase/Auth'
+pod 'Firebase/RemoteConfig'  
