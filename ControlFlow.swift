43c71357b913104e417e33574e86db5df2d7f079
iitjee <iit.jee.help.me@gmail.com>
2016-11-05 14:08:38 +0530

    Update ControlFlow.swift

--- ControlFlow.swift
+++ ControlFlow.swift
@@ -3,7 +3,7 @@ https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift
 
 1. Even for single if statement, you've to put braces
 2. Switch statements doesn't suffer from fallthrough
-3. It's mandatory to put a default case in Switch statements to make sure it's exhaustive.
+3. It's mandatory to put a default case(or a"general" case) in Switch statements to make sure it's exhaustive.
 
 */
 
@@ -49,28 +49,39 @@ switch someItem {
     default:
         print("It's not an alphabet")
 }
-//Note that there is no `break` in cases. Also, one can use the keyword `fallthrough` if you need that behavior
-
+//Note that there is no `break` in cases. Also, one can use the keyword `fallthrough` if you need that behavior (see below)
+//if you use "fallthrough" in every case, it's nothing but an if+else-if statement :)
 let someItem: Character = "w"
-
 switch someItem {
     case "a", "e", "i", "o", "u":
         print("Letter is vowel")
-    
     case "a"..."z":
         fallthrough
-  
     default:
         print("Letter is a consonant")
 }
 
+/* Switch-Case with Tuples and WHERE clause */
+let aPoint = (1.0, 2.0)
+switch aPoint {
+  case (let x, 2.0): print(x)
+  case (1.0, let y): print(y)
+  case let (x, y) where x>0.0 && y>0.0: print("\(y), \(x)")
+  case let (x, y): print("\(y), \(x)")  //note that this is equivalent to default but if u use default, you don't have labels for tuple members
+//(though you can get them by aPoint.1 and aPoint.9)
+}
 
 
+//if u want to use default statement, you could give labels to tuple members
+let aPoint = (x:1.0, y:2.0)
+switch aPoint {
+//...
+  default: print("\(aPoint.y), \(aPoint.x)")
+}
 
+//few left
 
-
-
-
+/* Switch-Case with Enums */
 
 
 
