a1add588eee509b7086a7b9c4624db316a221961
iitjee <iit.jee.help.me@gmail.com>
2016-11-05 22:06:11 +0530

    Update Functions.swift

--- Functions.swift
+++ Functions.swift
@@ -1,7 +1,8 @@
 /*
 1. By default, the parameters are `constants` unless declared with a var keyword
-2. You can also pass tuples as arguments func myF(x: (Int, Int))
-
+2. A function may have at most one variadic parameter
+3. In langs like C++ and C#, passing by reference is the only way to get multiple return values. However in swift we have
+   tuples. So In-Out parameters are generally not used and use only when required :)
 
 
 */
@@ -15,11 +16,13 @@ func greet(person: String) -> String {  //Note that person is a constant string
 }
 print(greet(person: "Anna"))
 
+/* No more valid, You can't have var keyword used in function arguments 
 func greet(var person: String) -> String {  //Note that person is now a variable string
     var person = "Hello, " + person + "!"
     return person
 }
 print(greet(person: "Anna"))
+/*
 
 /* Functions with no return values  */
 func greet(person: String) {
@@ -87,6 +90,7 @@ print(greet(person: "Bill", from: "Cupertino")) // Prints "Hello Bill!  Glad you
 //The use of argument labels can allow a function to be called in an expressive, sentence-like manner, while still providing a function body that is readable and clear in intent.
 
 //Note: If a parameter has an argument label, the argument must be labeled when you call the function.
+//Note2: There's no point if you put same names for both external and internal names. Infact you get a warning when u do so
 
 /*  Omitting Argument Labels and Parameter Names */
 func someFunction(_ firstParameterName: Int, secondParameterName: Int) {
@@ -117,8 +121,53 @@ func myOverload(a a:Int)
 func myOverload(b b:Int)
 func myOverload(c c:Int)
 
+/*  Tuples as arguments */
 
 
 
+/* Variadic Parameters  */
+//Note: A function may have at most one variadic parameter.
+func arithmeticMean(numbers: Double...) -> Double {
+    var total: Double = 0
+    for number in numbers {
+        total += number
+    }
+    return total / Double(numbers.count)
+}
+arithmeticMean(numbers: 1, 2, 3, 4, 5)   // returns 3.0, which is the arithmetic mean of these five numbers
+arithmeticMean(numbers: 3, 8.25, 18.75)  // returns 10.0, which is the arithmetic mean of these three numbers
 
+/*  In-Out parameters   */
+//They are an alternative way for a function to have an effect outside of the scope of its function body.
+func swapTwoInts(_ a: inout Int, _ b: inout Int) {  //this is the only case where argument variables are not constants
+    let temporaryA = a
+    a = b
+    b = temporaryA
+}
+
+var someInt = 3
+var anotherInt = 107
+swapTwoInts(&someInt, &anotherInt)  //You place an ampersand (&) directly before a variable’s name when you pass it as an argument to an in-out parameter, to indicate that it can be modified by the function.
+print("someInt is now \(someInt), and anotherInt is now \(anotherInt)")// Prints "someInt is now 107, and anotherInt is now 3"
+
+//Note: Usually not recommended
+//IMP: In-out parameters cannot have default values, and variadic parameters cannot be marked as inout.
+
+
+/*  An Enum example */
+enum Status {
+case success, failure
+}
+
+func doSomething(inout result: String) -> Status {
+    result = "New value"
+    return .success //Status.success is not mentioned because return type is already given in function declaration
+}
+//In this example, notice that we modify the variable passed to the function as well as return something! 
+//But this is not the best way to do so. When you want to return two variables use tuples instead of in-out like this
+
+func doSomething(Status: status, result: String) {
+    result = "New value"
+    return (.success,  result)
+}
 
