40a67eb48b0d577328f8d39102696d4ee90268f5
iitjee <iit.jee.help.me@gmail.com>
2016-11-06 22:55:40 +0530

    Update FunctionsClosures.swift

--- FunctionsClosures.swift
+++ FunctionsClosures.swift
@@ -5,7 +5,7 @@
 */
 
 
-/*  Function references */
+/*  Function references */  //Functions are "first-class" objects
 func sayHello() {print("Hello IIT")}
 var talk = sayHello //implicit type inference
 var talkAgain: ()->() = sayHello  //explicitly mentioning type
@@ -55,3 +55,56 @@ print("zero!")
 // -2...
 // -1...
 // zero!
+
+
+
+/*  Closures  */
+//Closures can capture and store references to any constants and variables from the context in which they are defined. 
+//This is known as closing over those constants and variables. 
+//Swift handles all of the memory management of capturing for you.
+
+(arguments) -> returnType in ClosureBody_NobracesReq
+
+func toDouble(x: Int) -> Double {  return Double(x) }
+//closure equivalents to above line
+let ref: (Int) -> Double =  {(x: Int)  -> Double in return Double(x) } //here we have explicitly mentioned the type of ref
+let ref = {(x: Int) -> Double in return Double(x)}  //type inference
+let ref = {(x: Int) in return Double(x)}  //return type never mentioned
+let ref: (Int) -> Double = {x in return Double(x)}  //type of x can be inferred from the mentioned signature of `ref`
+let ref: (Int) -> Double = {return Double($0)}  //from the signature it can be seen that there is one Int arg (acc by $0)
+let ref: (Int) -> Double = {Double($0)} //since function body has only one statement, `return` keyword can be omitted
+
+//Calling
+print(ref(2));
+
+
+/*  Global and nested functions are actually special cases of closures.
+Closures take one of three forms:
+Global functions are closures that have a name and do not capture any values.
+Nested functions are closures that have a name and can capture values from their enclosing function.
+Closure expressions are unnamed closures written in a lightweight syntax that can capture values from their surrounding context.
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
