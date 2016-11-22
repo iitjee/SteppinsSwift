767b98b2a0ca2465a1f33579f8385bdf19537739
iitjee <iit.jee.help.me@gmail.com>
2016-11-23 04:47:19 +0530

    Update Protocols.swift

--- Protocols.swift
+++ Protocols.swift
@@ -4,6 +4,10 @@ https://developer.apple.com/library/content/documentation/Swift/Conceptual/Swift
 A protocol defines a blueprint of methods, properties, and other requirements that suit a particular task
 Protocol == Interface (Adopting a protocol == Implementing an Interface)
 
+Protocols(Interfaces) are at the center of almost any programming language. They are better than inheritance due to a problem
+called "Fragile Base Class"
+
+
 
 
 */
@@ -31,7 +35,7 @@ class SomeClass: SomeSuperclass, FirstProtocol, AnotherProtocol {
 protocol SomeProtocol {
 //instance property requirements are always declared as variable properties, prefixed with the var keyword.
     var mustBeSettable: Int { get set }   //(gettable and settable) property
-    var doesNotNeedToBeSettable: Int { get }  //(gettable) property
+    var doesNotNeedToBeSettable: Int { get }  //(gettable) property(=> read-only) (also, for read-only, you can omit 'get')
 
 //Always prefix type property requirements with the static keyword when you define them in a protocol
     static var someTypeProperty: Int { get set }
@@ -46,7 +50,7 @@ protocol SomeProtocol {
     func random() -> Double //instance method
 }
 
-/*  Mutating Method Requirements  */
+/*  Mutating Method Requirements(ONY FOR STRUCTS AND ENUMS)  */
 //It is sometimes necessary for a method to modify (or mutate) the instance it belongs to.
 //The mutating keyword is only used by structures and enumerations. (i.e value types only)
 protocol Togglable {
@@ -93,7 +97,7 @@ class SomeSuperClass {
 }
 class SomeSubClass: SomeSuperClass, SomeProtocol {
     // "required" from SomeProtocol conformance; "override" from SomeSuperClass
-    required override init() {
+    required override init() {  //here since 'required' keyword is used, it's protocol's init() method
         // initializer implementation goes here
     }
 }
@@ -148,6 +152,22 @@ for _ in 1...5 {
 // Random dice roll is 4
 
 
+
+
+//A protocol can be used as the type to be stored in a collection such as an array or a dictionary,
+let things: [TextRepresentable] = [game, d12, simonTheHamster]  //TextRepresentable is the protocol
+for thing in things {
+    print(thing.textualDescription)
+}
+// A game of Snakes and Ladders with 25 squares
+// A 12-sided dice
+// A hamster named Simon
+
+
+
+
+
+
 /*  Protocol Inheritance  */
 //A protocol can inherit one or more other protocols and can add further requirements on top of the requirements it inherits.
 protocol InheritingProtocol: SomeProtocol, AnotherProtocol {
@@ -178,6 +198,146 @@ extension SnakesAndLadders: PrettyTextRepresentable {
     }
 }
 //Note that anything that is PrettyTextRepresentable must also be TextRepresentable, and so the implementation of prettyTextualDescription starts by accessing the textualDescription property from the TextRepresentable protocol to begin an output string. 
+
+/*  Class-Only Protocols    */
+//You can limit protocol adoption to class types (and not structures or enumerations) by adding the class keyword to a protocol’s inheritance list.
+//VERY IMP:Use a class-only protocol when the behavior defined by that protocol’s requirements assumes or requires that a conforming type has reference semantics rather than value semantics.
+protocol SomeClassOnlyProtocol: class, SomeInheritedProtocol {
+    // class-only protocol definition goes here
+}
+// It is a compile-time error to write a structure or enumeration definition that tries to adopt SomeClassOnlyProtocol.
+
+
+
+/* Protocol Composition */
+//You can combine multiple protocols into a single requirement with a protocol composition for eg; SomeProtocol & AnotherProtocol
+protocol Named {
+    var name: String { get }
+}
+protocol Aged {
+    var age: Int { get }
+}
+struct Person: Named, Aged {
+    var name: String
+    var age: Int
+}
+func wishHappyBirthday(to celebrator: Named & Aged) {
+    print("Happy birthday, \(celebrator.name), you're \(celebrator.age)!")
+}
+let birthdayPerson = Person(name: "Malcolm", age: 21)
+wishHappyBirthday(to: birthdayPerson)   // Prints "Happy birthday, Malcolm, you're 21!"
+//The type of the celebrator parameter is Named & Aged, which means “any type that conforms to both the Named and Aged protocols.” It doesn’t matter what specific type is passed to the function, as long as it conforms to both of the required protocols.
+//Protocol compositions do not define a new, permanent protocol type. Rather, they define a temporary local protocol that has the combined requirements of all protocols in the composition.
+
+/*  Checking for Protocol Conformance   */
+You can use the is and as operators described in Type Casting to check for protocol conformance, and to cast to a specific protocol.
+- The is operator returns true if an instance conforms to a protocol and returns false if it does not.
+- The as? version of the downcast operator returns an optional value of the protocol’s type, and this value is nil if the instance does not conform to that protocol.
+- The as! version of the downcast operator forces the downcast to the protocol type and triggers a runtime error if the downcast does not succeed.
+*/
+
+protocol HasArea {
+    var area: Double { get }
+}
+class Circle: HasArea {
+    let pi = 3.1415927
+    var radius: Double
+    var area: Double { return pi * radius * radius }
+    init(radius: Double) { self.radius = radius }
+}
+class Country: HasArea {
+    var area: Double
+    init(area: Double) { self.area = area }
+}
+//The Circle class implements the area property requirement as a computed property, based on a stored radius property. The Country class implements the area requirement directly as a stored property. Both classes correctly conform to the HasArea protocol.
+class Animal {
+    var legs: Int
+    init(legs: Int) { self.legs = legs }
+}
+
+let objects: [AnyObject] = [
+    Circle(radius: 2.0),
+    Country(area: 243_610),
+    Animal(legs: 4)
+]
+//The Circle, Country and Animal classes do not have a shared base class. Nonetheless, they are all classes, and so instances of all three types can be used to initialize an array that stores values of type AnyObject:
+//The objects array can now be iterated, and each object in the array can be checked to see if it conforms to the HasArea protocol:
+for object in objects {
+    if let objectWithArea = object as? HasArea {
+        print("Area is \(objectWithArea.area)")
+    } else {
+        print("Something that doesn't have an area")
+    }
+}
+// Area is 12.5663708
+// Area is 243610.0
+// Something that doesn't have an area
+
+//IMP NOTE: Note that the underlying objects are not changed by the casting process. They continue to be a Circle, a Country and an Animal. However, at the point that they are stored in the objectWithArea constant, they are only known to be of type HasArea, and so only their area property can be accessed.
+
+
+
+/*  Optional Protocol Requirements  */
+//Optional requirements are available so that you can write code that interoperates with Objective-C.
+// Both the protocol and the optional requirement must be marked with the @objc attribute.
+// Note that @objc protocols can be adopted only by classes that inherit from Objective-C classes or other @objc classes.
+// They can’t be adopted by structures or enumerations.
+
+//Note: When you use a method or property in an optional requirement, its type automatically becomes an optional. For example, a method of type (Int) -> String becomes ((Int) -> String)?. Note that the entire function type is wrapped in the optional, not the method’s return value.
+
+//An optional protocol requirement can be called with optional chaining, to account for the possibility that the requirement was not implemented by a type that conforms to the protocol using this syntax: someOptionalMethod?(someArgument) 
+@objc protocol CounterDataSource {
+    @objc optional func increment(forCount count: Int) -> Int
+    @objc optional var fixedIncrement: Int { get }
+}
+//The CounterDataSource protocol defines an optional method requirement called increment(forCount:) and an optional property requirement called fixedIncrement.
+//These requirements define two different ways for data sources to provide an appropriate increment amount for a Counter instance.
+
+class Counter {
+    var count = 0
+    var dataSource: CounterDataSource?
+    func increment() {
+        if let amount = dataSource?.increment?(forCount: count) {
+            count += amount
+        } else if let amount = dataSource?.fixedIncrement {
+            count += amount
+        }
+    }
+    //The increment() method first tries to retrieve an increment amount by looking for an implementation of the increment(forCount:) method on its data source. The increment() method uses optional chaining to try to call increment(forCount:), and passes the current count value as the method’s single argument.
+}
+
+//IMPORTANT: Note that two levels of optional chaining are at play here. First, it is possible that dataSource may be nil, and so dataSource has a question mark after its name to indicate that increment(forCount:) should be called only if dataSource isn’t nil. 
+//           Second, even if dataSource does exist, there is no guarantee that it implements increment(forCount:), because it is an optional requirement. Here, the possibility that increment(forCount:) might not be implemented is also handled by optional chaining. 
+//           The call to increment(forCount:) happens only if increment(forCount:) exists—that is, if it isn’t nil. This is why increment(forCount:) is also written with a question mark after its name.
+
+//Because the call to increment(forCount:) can fail for either of these two reasons, the call returns an optional Int value. This is true even though increment(forCount:) is defined as returning a nonoptional Int value in the definition of CounterDataSource.
+
+//After calling increment(forCount:), the optional Int that it returns is unwrapped into a constant called amount, using optional binding. If the optional Int does contain a value—that is, if the delegate and method both exist, and the method returned a value—the unwrapped amount is added onto the stored count property, and incrementation is complete.
+
+//If it is not possible to retrieve a value from the increment(forCount:) method—either because dataSource is nil, or because the data source does not implement increment(forCount:)—then the increment() method tries to retrieve a value from the data source’s fixedIncrement property instead. The fixedIncrement property is also an optional requirement, so its value is an optional Int value, even though fixedIncrement is defined as a nonoptional Int property as part of the CounterDataSource protocol definition.
+
+class ThreeSource: NSObject, CounterDataSource {
+    let fixedIncrement = 3
+}   //Here’s a simple CounterDataSource implementation where the data source returns a constant value of 3 every time it is queried. It does this by implementing the optional fixedIncrement property requirement
+
+var counter = Counter()
+counter.dataSource = ThreeSource()
+for _ in 1...4 {
+    counter.increment()
+    print(counter.count)
+}
+// 3
+// 6
+// 9
+// 12
+
+
+//Here’s a more complex data source called TowardsZeroSource, which makes a Counter instance count up or down towards zero from its current count value:
+
+to be contd..
+
+
+
 
 
 
