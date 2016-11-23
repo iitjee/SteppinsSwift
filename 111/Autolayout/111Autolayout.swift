e0a9592cb6ab1116bd3c91ee947cff5b229d589c
iitjee <iit.jee.help.me@gmail.com>
2016-11-23 19:14:00 +0530

    Create 111Autolayout.swift

---
+++ 111/Autolayout/111Autolayout.swift
@@ -0,0 +1,22 @@
+/*
+
+You "layout" a constraint! (nice semantics ;) )
+
+
+Shortcuts:
+1. When you take an image, click Cmd+Opt+= to get it to the right size
+*2. You can see a preview of any scene in assistant Editor -> @top you see two circles button -> click preview(last option)
+
+*/
+
+
+You can click on yellow/red signs in the doc outline for constraint warnings/errors and tada! you get a popup
+
+You can give labels to views in SB in Identity inspector @bottom (IDENTIFIER)
+You can also give labels to constrains in the Attrb inspector
+
+You can click 'edit' on constraints for a view in the Size inspector @Bottom
+
+
+
+
